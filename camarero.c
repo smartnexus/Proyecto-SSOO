610c689e8e323317438f2eaf8fdbdc0e7a14333d
smartnexus <angrodboh@alum.us.es>
2019-12-27 19:07:41 +0100

    123456

--- camarero.c
+++ camarero.c
@@ -1,1 +1,17 @@
-#include 
+#include "<stdio.h>"
+#include "<semaphore.h>"
+#include "<fcntl.h>"
+#include "<stdlib.h>"
+#include "<unistd.h>"
+
+int inicializar() {
+   //Abrir semaforo
+   sem_t *semID=NULL;
+   semID = sem_open("llamar_camarero", 0);
+}
+
+int main() {
+   inicializar();
+
+}
+
--- cliente.c
+++ cliente.c
@@ -1,20 +1,17 @@
 #include "<stdio.h>"
+#include "<semaphore.h>"
+#include "<fcntl.h>"
+#include "<stdlib.h>"
+#include "<unistd.h>"
 
-
-
+int inicializar() {
+   //Abrir semaforo
+   sem_t *semID=NULL;
+   semID = sem_open("llamar_camarero", 0);
+}
 
 int main(){
-
-
-  //Abrir semaforo
-
-  //
-
-
-
-
-
-  
+   inicializar();
 
 
 
