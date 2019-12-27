c93a05ccf4d60250010122316722f9d600a21b79
smartnexus <angrodboh@alum.us.es>
2019-12-27 20:05:55 +0100

    Chiclín

--- camarero.c
+++ camarero.c
@@ -4,14 +4,22 @@
 #include "<stdlib.h>"
 #include "<unistd.h>"
 
+key_t clave;
+sem_t *llamar=NULL;
+int msgqueue_id;
+
 int inicializar() {
    //Abrir semaforo
-   sem_t *semID=NULL;
-   semID = sem_open("llamar_camarero", 0);
+   llamar = sem_open("llamar_camarero", 0);
+   clave=ftok(".",'m');
+   if ((msgqueue_id=msgget(clave,IPC_CREAT|0660))==-1) {
+      printf("Error al iniciar la cola\n");
+   }
 }
 
 int main() {
    inicializar();
+   //TODO: Hacer bucle esperando mensaje de pedir de un cliente.
 
 }
 
--- cliente.c
+++ cliente.c
@@ -1,18 +1,40 @@
-#include "<stdio.h>"
-#include "<semaphore.h>"
-#include "<fcntl.h>"
-#include "<stdlib.h>"
-#include "<unistd.h>"
+#include <sys/wait.h> 
+#include <stdio.h> 
+#include <unistd.h>
+#include <stdlib.h> 
+#include <ctype.h> 
+#include <sys/msg.h> 
+#include <string.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/msg.h>
+#include <sys/shm.h>
+
+key_t clave;
+sem_t *llamar=NULL;
+int msgqueue_id;
 
 int inicializar() {
    //Abrir semaforo
-   sem_t *semID=NULL;
-   semID = sem_open("llamar_camarero", 0);
+   llamar = sem_open("llamar_camarero", 0);
+   clave=ftok(".",'m');
+   if ((msgqueue_id=msgget(clave,IPC_CREAT|0660))==-1) {
+      printf("Error al iniciar la cola\n");
+   }
 }
 
-int main(){
+int main() {
    inicializar();
-
-
-
+   printf("¡Bienvenido!\n");
+   //TODO: Hacer bucle para que no se salga del programa cuando se pulsa la tecla incorrecta.
+   printf("Pulse Enter para llamar al camarero:");
+   char ch=fgetc(stdin);
+   if(ch==0x0A) {
+      printf("Se ha llamado al camarero, espere por favor.\n");
+      sem_wait(llamar);
+      //TODO: Enviar mensaje de pedir por cola y recibir menú.
+   } else {
+      printf("La entrada introducida es incorrecta.\n");
+   }
 }
