4337525b254b6aad632dc1f469dca951d66a67a2
jacjurtab <59292552+jacjurtab@users.noreply.github.com>
2019-12-27 19:49:26 +0100

    Create cocinero.c

---
+++ cocinero.c
@@ -0,0 +1,17 @@
+# Proyecto-SSOO
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <ctype.h> 
+#include <sys/msg.h> 
+#include <string.h>
+#include <unistd.h>
+
+
+
+int main(int argc, char** argv){
+
+
+
+
+  return 0;
+}
